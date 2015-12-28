404d4d366ac1fb3191bc1d727ba8b8ae9f597f6c
Marco <marco.sarmiento1120@gmail.com>
2015-12-28 02:34:36 -0700

    Optimizations + APK

--- mobile-config.js
+++ mobile-config.js
@@ -4,7 +4,8 @@ App.info({
   description: 'Future Business Leaders of American 2015-2016 Mobile APP Entry',
   author: 'PopCode',
   email: 'marcosarmiento1120@gmail.com',
-  website: 'http://macrosacramento.github.io'
+  website: 'http://macrosacramento.github.io',
+  version: '0.9.0'
 });
 
 App.icons({
